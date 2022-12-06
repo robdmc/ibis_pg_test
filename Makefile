63adca35f346113304bbee7f45cdfb4218f7afda
Rob deCarvalho <rob@rob.net>
2022-12-06 17:47:16 -0500

    initial code import

---
+++ Makefile
@@ -0,0 +1,23 @@
+#! /usr/bin/make 
+
+
+.PHONY: help \
+shell \
+reset
+
+help:  ## Print the help documentation
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+shell: ## Open a shell in the container.  Can run ipython from here
+	-docker-compose run --rm shell
+
+test: ## Run failing ibis scenario
+	-docker-compose run --rm test
+
+reset: ## Only reset docker stuff for this project
+	docker-compose down
+	-docker images | grep 'ibis_test_base' | awk '{print $3}'  | xargs docker rmi
+	-docker builder prune -f
+
+
+
---
+++ doit.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import os
+import ibis
+import pandas as pd
+
+def get_postgres_creds():
+    kwargs = {
+        'host': os.environ['PGHOST'],
+        'user': os.environ['PGUSER'],
+        'password': os.environ['PGPASSWORD'],
+        'database': os.environ['PGDATABASE']
+    }
+    return kwargs
+
+
+def get_postgres_ibis_connection():
+    kwargs = get_postgres_creds()
+    conn = ibis.postgres.connect(**kwargs)
+    return conn
+
+
+conn = get_postgres_ibis_connection()
+conn.raw_sql("drop table if exists test")
+
+# Both integer and uuid currently failing
+try_uuid = False
+if try_uuid:
+    conn.raw_sql("create table test (id uuid, x float, PRIMARY KEY (id))")
+else:
+    conn.raw_sql("create table test (id integer, x float, PRIMARY KEY (id))")
+
+df = pd.DataFrame({'x': [1., 2., 3.]})
+
+conn.insert('test', df)
