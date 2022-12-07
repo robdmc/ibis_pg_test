680194eba167c710ba4dd0aa3ac01dca56b50350
Rob deCarvalho <rob@rob.net>
2022-12-07 11:34:27 -0500

    changed how table is created to make things work

--- doit_doesnt_work.py
+++ doit_doesnt_work.py
@@ -23,12 +23,17 @@ def get_postgres_ibis_connection():
 conn = get_postgres_ibis_connection()
 conn.raw_sql("drop table if exists test")
 
-conn.raw_sql("create table test (id bigserial primary key, x float)")
+# Both integer and uuid currently failing
+try_uuid = False
+if try_uuid:
+    conn.raw_sql("create table test (id uuid, x float, PRIMARY KEY (id))")
+else:
+    conn.raw_sql("create table test (id integer, x float, PRIMARY KEY (id))")
 
-for nn in range(10):
-    df = pd.DataFrame({'x': [1., 2., 3.]})
-    conn.insert('test', df)
+# Works for integer primary key
+df = pd.DataFrame({'id': [1, 2, 3], 'x': [1., 2., 3.]})
+conn.insert('test', df)
 
-t = conn.table('test')
-print()
-print(t.execute().to_string())
+# Fails
+df = pd.DataFrame({'x': [1., 2., 3.]})
+conn.insert('test', df)
